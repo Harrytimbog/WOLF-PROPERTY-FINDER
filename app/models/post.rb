53549dfae810c68ff3ce68353c3180c252498e93
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-05-06 12:53:06 +0100

    set up image display logic for a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :image
-  validates :title, :summary, :body, :image, presence: true
+  validates :title, :summary, :body, presence: true
 
   scope :active, -> { where(active: true) }
   scope :latest, -> { order created_at: :desc }
